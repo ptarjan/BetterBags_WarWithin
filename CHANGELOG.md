14584fcdbb7d5af4fff5b2564115396b022ec951
Lucas Vienna <dev@lucasvienna.dev>
2026-03-04 19:11:32 +0100

    Improve pcall with debug logging

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 - Bump TOC to 12.0.1
 
+### Fixed
+- Remove deleted items
+
 ## [1.12.0] 2026-01-22
 ### Changed
 - Bump TOC to 12.0.0
--- main.lua
+++ main.lua
@@ -22,8 +22,9 @@ local ctx = context:New('BBTWW_Event')
 for category, items in pairs(addon.db) do
 	categories:WipeCategory(ctx, L:G(category))
 	for _, item in pairs(items) do
-		-- pcall to skip items that no longer exist in the current client
-		-- (e.g. seasonal items not yet active or removed between patches)
-		pcall(categories.AddItemToCategory, categories, ctx, item, L:G(category))
+		local success, err = pcall(categories.AddItemToCategory, categories, ctx, item, L:G(category))
+		--@debug@
+		if not success then print(err) end
+		--@end-debug@
 	end
 end
